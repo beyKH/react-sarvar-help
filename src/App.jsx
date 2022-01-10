90ce33a3bbf561cba09686f226a0bc581867b88a
beyKH <asadbek.mah@gmail.com>
2022-01-10 16:40:58 +0500

    done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className='main__container'>
+      <h1 className='main__heading'>Almubs App</h1>
+
+    </div>
+    </>
+  );
+}
+
+export default App;
